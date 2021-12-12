14c5e2437bd9ac8b113b64fd683f53c27dd5dcff
Ducdv <duc.dv@haposoft.com>
2021-12-12 17:04:38 +0700

    1.2 Write your actual test

---
+++ tests/Unit/ArticleApiUnitTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class ArticleApiUnitTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function itCanCreateAnArticle()
+    {
+        $data = [
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraph
+        ];
+
+        $this->post(route('articles.store'), $data)
+            ->dump()
+            ->assertStatus(201)
+            ->assertJson($data);
+    }
+}
